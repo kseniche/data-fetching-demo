b38fab7da39b52b802cd8175b960629d96512017
Чеботарёва Ксения Евгеньевна <22049@iat.iat>
2024-10-24 13:06:30 +0800

    Updates

---
+++ static/js/246.3b2e6c2c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktodo_app=self.webpackChunktodo_app||[]).push([[246],{246:(e,s,l)=>{l.r(s),l.d(s,{default:()=>d});var o=l(43),t=l(579);const d=o.memo((e=>{let{post:s}=e;return console.log("\u0420\u0435\u043d\u0434\u0435\u0440 \u043f\u043e\u0441\u0442\u0430:",s.id),(0,t.jsxs)("li",{children:[(0,t.jsx)("h3",{children:s.title}),(0,t.jsx)("p",{children:s.body})]})}))}}]);
+//# sourceMappingURL=246.3b2e6c2c.chunk.js.map
